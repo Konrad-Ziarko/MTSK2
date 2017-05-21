025a585186de5a37f659954858ff9dab948ebc0e
Zoufland <zoufland@gmail.com>
2017-05-21 19:40:11 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by konrad on 5/21/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
