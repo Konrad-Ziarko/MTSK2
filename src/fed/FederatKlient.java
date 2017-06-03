2b16249b09ff68907ffcc2e6683c8a1c183424a7
konrad <konrad19931118@gmail.com>
2017-06-03 18:07:05 +0200

    Modyfikacja federata klienta

--- src/fed/FederatKlient.java
+++ src/fed/FederatKlient.java
@@ -4,11 +4,8 @@ package fed;
  */
 
 import amb.Ambasador;
+import fom.FomInteraction;
 import hla.rti.*;
-import hla.rti.jlc.EncodingHelpers;
-import hla.rti.jlc.RtiFactoryFactory;
-
-import java.util.Collections;
 
 public class FederatKlient extends AbstractFederat {
     private int klientHandle;
@@ -32,37 +29,58 @@ public class FederatKlient extends AbstractFederat {
 
         while (fedamb.running) {
             double timeToAdvance = fedamb.federateTime + timeStep;
+
+            //logika obslugi wszystkich klientow
+
             advanceTime(timeToAdvance);
-
-            if (fedamb.externalEvents.size() > 0) {
-                Collections.sort(fedamb.externalEvents, new ExternalEvent.ExternalEventComparator());
-                for (ExternalEvent externalEvent : fedamb.externalEvents) {
-                    fedamb.federateTime = externalEvent.getTime();
-                    switch (externalEvent.getEventType()) {
-                        case ADD:
-                            this.addToStock(externalEvent.getQty());
-                            break;
-                    }
-                }
-                fedamb.externalEvents.clear();
-            }
-
-            if (fedamb.grantedTime == timeToAdvance) {
-                timeToAdvance += fedamb.federateLookahead;
-                log("Updating stock at time: " + timeToAdvance);
-                updateHLAObject(timeToAdvance);
-                fedamb.federateTime = timeToAdvance;
-            }
-            try {
-                rtiamb.tick();
-            } catch (RTIinternalError | ConcurrentAccessAttempted rtIinternalError) {
-                rtIinternalError.printStackTrace();
-            }
         }
     }
 
-    private void updateCheckoutQueueWith(int objectHandle, byte[] value) throws ArrayIndexOutOfBounds {
-        checkoutsQueueSizes.put(objectHandle, EncodingHelpers.decodeInt(value));
+    public void publishAndSubscribe() {
+        try {
+
+            int addQueueEntryHandle = rtiamb.getInteractionClassHandle("InteractionRoot.wejscieDoKolejki");
+            FomInteraction interaction = new FomInteraction(addQueueEntryHandle);
+            interaction.addAttributeHandle(NR_KASY, addQueueEntryHandle, Integer.class);
+            interaction.addAttributeHandle(NR_KLIENTA, addQueueEntryHandle, Integer.class);
+            rtiamb.publishInteractionClass(addQueueEntryHandle);
+
+            int addQueueExitHandle = rtiamb.getInteractionClassHandle("InteractionRoot.opuszczenieKolejki");
+            interaction = new FomInteraction(addQueueExitHandle);
+            interaction.addAttributeHandle(NR_KASY, addQueueEntryHandle, Integer.class);
+            interaction.addAttributeHandle(NR_KLIENTA, addQueueEntryHandle, Integer.class);
+            rtiamb.publishInteractionClass(addQueueExitHandle);
+
+            int addServicedHandle = rtiamb.getInteractionClassHandle("InteractionRoot.obsluzonoKlienta");
+            interaction = new FomInteraction(addServicedHandle);
+            interaction.addAttributeHandle(NR_KASY, addQueueEntryHandle, Integer.class);
+            interaction.addAttributeHandle(NR_KLIENTA, addQueueEntryHandle, Integer.class);
+            rtiamb.subscribeInteractionClass(addServicedHandle);
+
+            int addNewClientHandle = rtiamb.getInteractionClassHandle("InteractionRoot.nowyKlient");
+            interaction = new FomInteraction(addNewClientHandle);
+            interaction.addAttributeHandle(NR_KLIENTA, addQueueEntryHandle, Integer.class);
+            rtiamb.subscribeInteractionClass(addNewClientHandle);
+
+            int addCashEntryHandle = rtiamb.getInteractionClassHandle("InteractionRoot.wejscieDoKasy");
+            interaction = new FomInteraction(addCashEntryHandle);
+            interaction.addAttributeHandle(NR_KASY, addQueueEntryHandle, Integer.class);
+            interaction.addAttributeHandle(NR_KLIENTA, addQueueEntryHandle, Integer.class);
+            rtiamb.subscribeInteractionClass(addCashEntryHandle);
+
+            int addSimulationStartHandle = rtiamb.getInteractionClassHandle("InteractionRoot.startSymulacji");
+            rtiamb.subscribeInteractionClass(addSimulationStartHandle);
+
+            int addSimulationStopHandle = rtiamb.getInteractionClassHandle("InteractionRoot.stopSymulacji");
+            rtiamb.subscribeInteractionClass(addSimulationStopHandle);
+        } catch
+                (NameNotFound | FederateNotExecutionMember | RTIinternalError | InteractionClassNotDefined | SaveInProgress | ConcurrentAccessAttempted | RestoreInProgress | FederateLoggingServiceCalls
+                        nameNotFound) {
+            nameNotFound.printStackTrace();
+        }
+    }
+
+    public void deleteObjects() {
     }
 
     /*protected void deleteObjects() throws RTIexception {
@@ -75,53 +93,8 @@ public class FederatKlient extends AbstractFederat {
             }
         });
     }*/
-
-    public String toString() {
-        return "KlientFederate [bankCustomers[" + shoppingCustomers.size() + "], checkoutsQueueSizes["
-                + checkoutsQueueSizes.size() + "]=" + checkoutsQueueSizes + "]";
+    public void registerObjects() {
     }
-
-    private boolean isAttributeQueueLength(int attributeHandle, Class<?> typeFor) {
-        return typeFor.getName().equalsIgnoreCase(Integer.class.getName())
-                && kasaClassHandle.getNameFor(attributeHandle).equalsIgnoreCase(DLUGOSC_KOLEJKI);
-    }
-
-    public void publishAndSubscribe() {
-        int classHandle = 0;
-        try {
-            classHandle = rtiamb.getObjectClassHandle("ObjectRoot.Klient");
-            int stockHandle = rtiamb.getAttributeHandle("nrKlienta", classHandle);
-            AttributeHandleSet attributes =
-                    RtiFactoryFactory.getRtiFactory().createAttributeHandleSet();
-            attributes.add(stockHandle);
-
-            rtiamb.publishObjectClass(classHandle, attributes);
-
-            int addQueueEntryHandle = rtiamb.getInteractionClassHandle("InteractionRoot.wejscieDoKolejki");
-            rtiamb.publishInteractionClass(addQueueEntryHandle);
-            
-            int addQueueExitHandle = rtiamb.getInteractionClassHandle("InteractionRoot.opuszczenieKolejki");
-            rtiamb.publishInteractionClass(addQueueExitHandle);
-
-            int addServicedHandle = rtiamb.getInteractionClassHandle("InteractionRoot.obsluzonoKlienta");
-            rtiamb.subscribeInteractionClass(addServicedHandle);
-
-            int addNewClientHandle = rtiamb.getInteractionClassHandle("InteractionRoot.nowyKlient");
-            rtiamb.subscribeInteractionClass(addNewClientHandle);
-
-            int addCashEntryHandle = rtiamb.getInteractionClassHandle("InteractionRoot.wejscieDoKasy");
-            rtiamb.subscribeInteractionClass(addCashEntryHandle);
-
-            int addSimulationStartHandle = rtiamb.getInteractionClassHandle("InteractionRoot.startSymulacji");
-            rtiamb.subscribeInteractionClass(addSimulationStartHandle);
-
-            int addSimulationStopHandle = rtiamb.getInteractionClassHandle("InteractionRoot.stopSymulacji");
-            rtiamb.subscribeInteractionClass(addSimulationStopHandle);
-        } catch (NameNotFound | FederateNotExecutionMember | RTIinternalError | AttributeNotDefined | OwnershipAcquisitionPending | InteractionClassNotDefined | SaveInProgress | ConcurrentAccessAttempted | RestoreInProgress | FederateLoggingServiceCalls | ObjectClassNotDefined nameNotFound) {
-            nameNotFound.printStackTrace();
-        }
-    }
-
     /*public void registerObjects() {
         int classHandle = 0;
         try {
